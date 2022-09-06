88160759a82102559c9fdf1f492f9729a95eb4b6
Andrey Zemlyachev <kvintilian@gmail.com>
2022-09-06 22:09:20 +0300

    Update selectListByFilter.js

--- src/selectors/selectListByFilter.js
+++ src/selectors/selectListByFilter.js
@@ -4,7 +4,6 @@ export const selectListByFilter = createSelector(
   (state) => state.serviceList,
   (state) => state.serviceFilter,
   (seviceList, serviceFilter) => {
-    console.log("filterService", serviceFilter);
     return seviceList.filter((item) =>
       new RegExp(serviceFilter, "gi").test(item.name)
     );
